a088f07620e88d70d01958d12370b7b7d455f954
ricardoboso81 <64757142+ricardoboso81@users.noreply.github.com>
2020-06-10 18:01:43 -0700

    Update correos.php

--- correos.php
+++ correos.php
@@ -1,15 +1,15 @@
 <?php
-$desinatario ='lioncaro81@hotmail.com';
+$desinatario ='Lioncaro81@hotmail.com';
 // ES AL AL CORREO QUE SE ENVIARA//
 $nombre=$_POST['nombre'];
 $asunto=$_POST['asunto'];
 $mensaje=$_POST['mensaje'];
 $email=$_POST['email'];
-
+ 
 $header="Enviado desde la pagina de Ricardo Bojorquez";
-$mensajeCompleto=$mensaje. "\nAtentamente: ". $nombre;
+$mensajeCompleto= $mensaje. "\nAtentamente: ". $nombre;
 
-mail($desinatario,$asunto,$mensajeCompleto,$header);
+mail( $desinatario, $asunto, $mensajeCompleto, $header);
 echo"<script> alert('Correo envia satifactoriamente')</script>";
 echo"<script>setTimeout(\"location.href='index.html'\",1000)</script>";
 
